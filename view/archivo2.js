fb8d99810bafbea92b152888cf46b6c8f38d631b
mariam2121 <112910300+mariam2121@users.noreply.github.com>
2023-05-22 10:55:36 -0300

    Add files via upload

---
+++ view/archivo2.js
@@ -0,0 +1,10 @@
+const express = ('express');
+const app = ('');
+const bodyparse = ('body-parse');
+const ejs = ('ejs');
+const port = 2500;
+
+app.set('view engine','ejs');
+app.set('view',__dirname + '/view');
+
+app.post();
